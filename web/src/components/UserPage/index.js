dd64bea3936abca4eae6d6b5f253774032d28b0b
tloriato <tloriatosimoes@gmail.com>
2019-08-13 13:54:09 -0300

    Adding SidePanel

--- web/src/components/UserPage/index.js
+++ web/src/components/UserPage/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import SidePanel from '../SidePanel';
+import { UikContainerHorizontal, UikWidget } from '../../@uik';
 
 const UserPage = () => {
-  const a = 'oi';
+  const fillHeight = { height: '-webkit-fill-available' };
 
   return (
     <div>
-      <img src='#' />
-      a
+      <UikContainerHorizontal style={fillHeight}>
+        <SidePanel />
+        <UikWidget padding>
+          <h2>
+            This is a widget
+          </h2>
+        </UikWidget>
+      </UikContainerHorizontal>
     </div>
   );
 };
