6fc1bde3f297c839b87d7863a4a9fc5f06f2361e
David Beyda <dbeyda@globo.com>
2019-09-06 20:25:43 -0300

    [web] ListView Component

---
+++ web/src/components/ListView/index.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import {
+  UikWidget, UikWidgetHeader, UikWidgetContent, UikButton,
+  UikWidgetTable,
+} from '../../@uik';
+import api from '../../utils/api';
+import './style.scss';
+
+
+const ListView = (props) => {
+  const [tableData, setTableData] = useState([]);
+  const {
+    headers, endpoint, title, keyField,
+    buttonText, onClickAdd,
+  } = props;
+
+
+  useEffect(() => {
+    async function loadFromServer() {
+      const c = await api.get(endpoint);
+      console.log(`Just got ${title}`, c.data.data);
+      setTableData(c.data.data);
+    }
+    loadFromServer();
+  }, []);
+
+  const getColumnTitle = (header) => {
+    if (Array.isArray(header)) {
+      if (header.length > 1) return header[1];
+      return header[0];
+    }
+    return header;
+  };
+
+  const getColumnField = (header) => {
+    if (Array.isArray(header)) {
+      return header[0];
+    }
+    return header;
+  };
+
+  const h = () => headers.map(header => <th
+    key={getColumnField(header)}>{getColumnTitle(header)}</th>);
+
+  const rows = () => tableData.map(row => (
+    <tr key={row[keyField]}>
+      {headers.map(header => <td
+        key={`${getColumnField(header)}-${row[keyField]}`}>{row[getColumnField(header)]}</td>)}
+    </tr>
+  ));
+
+
+  return (
+    <>
+      <UikWidget className='widget-wrapper'>
+        <UikWidgetHeader
+          rightEl={(
+            <UikButton primary onClick={onClickAdd}>
+              {buttonText}
+            </UikButton>
+    )}
+  >
+          {title}
+        </UikWidgetHeader>
+        <UikWidgetContent>
+          <UikWidgetTable>
+            <thead>
+              <tr>
+                {h()}
+              </tr>
+            </thead>
+            <tbody>
+              {rows()}
+            </tbody>
+          </UikWidgetTable>
+        </UikWidgetContent>
+      </UikWidget>
+    </>
+  );
+};
+
+export default ListView;
