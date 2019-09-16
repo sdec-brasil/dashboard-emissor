0ceea94d3deb4a956f154ecfd2eeedd20ffc5f91
David Beyda <dbeyda@globo.com>
2019-09-16 13:54:23 -0300

    Removing old admin folder

--- admin/src/components/Block/index.js
+++
@@ -1,9 +0,0 @@
-import BlockIcon from '@material-ui/icons/CropSquare';
-import BlockShow from './BlockShow';
-import BlockList from './BlockList';
-
-export default {
-  list: BlockList,
-  show: BlockShow,
-  icon: BlockIcon,
-};
--- admin/src/components/City/index.js
+++
@@ -1,9 +0,0 @@
-import CityIcon from '@material-ui/icons/LocationCity';
-import CityShow from './CityShow';
-import CityList from './CityList';
-
-export default {
-  list: CityList,
-  show: CityShow,
-  icon: CityIcon,
-};
