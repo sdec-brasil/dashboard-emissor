50fea567c99e4b75dc93ee6453d24a68edbf3709
David Beyda <dbeyda@globo.com>
2019-09-17 13:59:56 -0300

    [api] Updating new-invoice validator

--- api/src/services/invoices/validators.js
+++ api/src/services/invoices/validators.js
@@ -3,12 +3,17 @@ import models from '../../models';
 
 const validators = {};
 validators.postInvoice = [
+  body('nonce').not().exists(),
+  body('invoiceCode').not().exists(),
   body('substitutes').not().exists(),
   body('substitutedBy').not().exists(),
-  body('enderecoEmissor').not().exists()
+  body('invoiceName').not().exists(),
+  body('status').not().exists(),
+  body('encryptedBorrower').isString().optional(),
+  body('emitter').exists()
     .custom((value, { req }) => {
       models.address.findByPk(value).then((addr) => {
-        if (addr === null) throw new Error('enderecoEmissor não encontrado no sistema.');
+        if (addr === null) throw new Error('emitter não encontrado no sistema.');
         models.empresa.findOne({ where: { walletId: addr.get('walletId') } })
           .then((empresa) => {
             if (empresa === null) throw new Error('enderecoEmissor ainda não foi autorizado por nenhuma empresa.');
@@ -16,7 +21,13 @@ validators.postInvoice = [
           });
       });
     }),
-  body('prestacao.prefeituraIncidencia', 'valor nao valido').isNumeric()
+
+  body('taxNumber').isString(),
+  // TODO: some validation required for taxNumber
+  body('blockHeight').not().exists(),
+
+  body('provision.issuedOn').isISO8601({ strict: true }),
+  body('provision.cityServiceLocation', 'valor invalido').isNumeric()
     .isLength({ min: 7, max: 7 })
     .custom(async (value, { req }) => {
       // TODO: should this be validated?
@@ -30,113 +41,104 @@ validators.postInvoice = [
       //   throw new Error('prefeitura não encontrada no sistema');
       // }
     }),
-  body('prestacao.baseCalculo', 'O valor esta incorreto')
-    .isInt()
+  body('provision.serviceCode', 'Deve ser uma string de até 20 caracteres')
+    .isString().isLength({ max: 20 }).optional(),
+  body('provision.codCnae', 'Deve ser uma string de até 20 caracteres')
+    .isString().isLength({ max: 20 }).optional(),
+  body('provision.codNBS', 'Deve ser uma string de até 9 caracteres')
+    .isString().isLength({ max: 9 }).optional(),
+  body('provision.description', 'Deve ser uma string de até 2000 caracteres')
+    .isString().isLength({ max: 2000 }),
+  body('provision.servicesAmount').isInt(),
+
+  body('tributes.incentivoFiscal')
+    .isBoolean(),
+  body('tributes.issExigibility', 'Deve ser um inteiro entre 1 e 7')
+    .isInt({ min: 1, max: 7 }),
+  body('tributes.processNumber', 'Deve ser uma string de até 30 caracteres')
+    .isString().isLength({ max: 30 }).optional(),
+  body('tributes.issRate').isNumeric(),
+  body('tributes.issAmount').isInt(),
+  body('tributes.issWithheld').isBoolean(),
+  body('tributes.retentionResponsible')
+    .isInt({ min: 1, max: 2 })
+    .optional(),
+  body('tributes.specialTaxRegime')
+    .isInt({ min: 0, max: 16 }).optional(),
+  body('tributes.pisAmount').isInt().optional(),
+  body('tributes.cofinsAmount').isInt().optional(),
+  body('tributes.inssAmount').isInt().optional(),
+  body('tributes.irAmount').isInt().optional(),
+  body('tributes.csllAmount').isInt().optional(),
+  body('tributes.unconditionedDiscountAmount').isInt().optional(),
+  body('tributes.conditionedDiscountAmount').isInt().optional(),
+  body('tributes.othersAmountsWithheld').isInt().optional(),
+  body('tributes.deductionsAmount').isInt().optional(),
+  body('tributes.calculationBasis', 'O valor esta incorreto').isInt()
     .custom(async (value, { req }) => {
       const data = req.body;
-      let targetValue = parseInt(data.prestacao.valServicos, 10) || 0;
-      targetValue -= parseInt(data.prestacao.valDeducoes, 10) || 0;
-      targetValue -= parseInt(data.prestacao.descontoIncond, 10) || 0;
+      let targetValue = parseInt(data.provision.servicesAmount, 10) || 0;
+      targetValue -= parseInt(data.tributes.deductionsAmount, 10) || 0;
+      targetValue -= parseInt(data.tributes.unconditionedDiscountAmount, 10) || 0;
       if (parseInt(value, 10) !== targetValue) {
         throw new Error('A base de calculo está errada.');
       }
     }),
-  body('prestacao.aliqServicos')
-    .isNumeric(),
-  body('prestacao.valLiquiNfse')
+  body('tributes.approximateTax').isInt().optional(),
+  body('tributes.netValueNfse')
     .isInt()
     .custom(async (value, { req }) => {
       const data = req.body;
-      let targetValue = (parseInt(data.prestacao.valServicos, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valPis, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valCofins, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valInss, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valIr, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valCsll, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.outrasRetencoes, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.valIss, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.descontoIncond, 10) || 0);
-      targetValue -= (parseInt(data.prestacao.descontoCond, 10) || 0);
+      let targetValue = (parseInt(data.provision.servicesAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.pisAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.cofinsAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.inssAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.irAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.csllAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.othersAmountsWithheld, 10) || 0);
+      targetValue -= (parseInt(data.tributes.issAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.unconditionedDiscountAmount, 10) || 0);
+      targetValue -= (parseInt(data.tributes.conditionedDiscountAmount, 10) || 0);
       if (parseInt(value, 10) !== targetValue) {
         throw new Error('ValLiquiNfse está incorreto');
       }
     }),
-  body('prestacao.dataIncidencia').isInt(),
-  body('prestacao.valServicos').isInt(),
-  body('prestacao.valDeducoes').isInt().optional(),
-  body('prestacao.valPis').isInt().optional(),
-  body('prestacao.valCofins').isInt().optional(),
-  body('prestacao.valInss').isInt().optional(),
-  body('prestacao.valIr').isInt().optional(),
-  body('prestacao.valCsll').isInt().optional(),
-  body('prestacao.outrasRetencoes').isInt().optional(),
-  body('prestacao.valTotalTributos').isInt().optional(),
-  body('prestacao.valIss').isInt(),
-  body('prestacao.descontoIncond').isInt().optional(),
-  body('prestacao.descontoCond').isInt().optional(),
-  body('prestacao.issRetido').isBoolean(),
-  body('prestacao.respRetencao')
-    .isInt({ min: 1, max: 2 })
-    .optional(),
-  body('prestacao.itemLista', 'Deve ser uma string de até 5 caracteres')
-    .isString().isLength({ max: 5 }),
-  body('prestacao.codCnae', 'Deve ser uma string de até 20 caracteres')
-    .isString().isLength({ max: 20 }).optional(),
-  body('prestacao.codServico', 'Deve ser uma string de até 20 caracteres')
-    .isString().isLength({ max: 20 }).optional(),
-  body('prestacao.codNBS', 'Deve ser uma string de até 9 caracteres')
-    .isString().isLength({ max: 9 }).optional(),
-  body('prestacao.discriminacao', 'Deve ser uma string de até 2000 caracteres')
-    .isString().isLength({ max: 2000 }),
-  body('prestacao.exigibilidadeISS', 'Deve ser um inteiro entre 1 e 7')
-    .isInt({ min: 1, max: 7 }),
-  body('prestacao.numProcesso', 'Deve ser uma string de até 30 caracteres')
-    .isString().isLength({ max: 30 }).optional(),
-  body('prestacao.regimeEspTribut')
-    .isInt({ min: 1, max: 6 }).optional(),
-  body('prestacao.optanteSimplesNacional', 'valor deve ser true ou false')
-    .isBoolean(),
-  body('prestacao.incentivoFiscal')
-    .isBoolean(),
-  body('tomador.identificacaoTomador')
-    .isString().isLength({ max: 14 }).optional(),
-  body('tomador.nif')
-    .isString().isLength({ max: 40 }).optional(),
-  body('tomador.nomeRazaoTomador')
+  body('borrower.taxNumber').isString().isLength({ max: 14 }).optional(),
+  body('borrower.nif').isString().isLength({ max: 40 }).optional(),
+  body('borrower.name')
     .isString().isLength({ max: 150 }).optional(),
-  body('tomador.logEnd')
+  body('borrower.street')
     .isString().isLength({ max: 125 }).optional(),
-  body('tomador.nif')
-    .isString().isLength({ max: 40 }).optional(),
-  body('tomador.numEnd')
+  body('borrower.number')
     .isString().isLength({ max: 10 })
     .optional(),
-  body('tomador.compEnd').optional()
+  body('borrower.additionalInformation').optional()
     .isString().isLength({ max: 60 })
     .optional(),
-  body('tomador.bairroEnd')
+  body('borrower.district')
     .isString().isLength({ max: 60 }).optional(),
-  body('tomador.cidadeEnd')
+  body('borrower.city')
     .isInt().optional(),
-  body('tomador.estadoEnd')
+  body('borrower.state')
     .isString().isLength({ max: 2 }).optional(),
-  body('tomador.paisEnd')
+  body('borrower.country')
     .isInt().optional(),
-  body('tomador.cepEnd')
+  body('borrower.postalCode')
     .isString().isLength({ max: 8 }).optional(),
-  body('tomador.email')
+  body('borrower.email')
     .isString().isEmail().optional(),
-  body('tomador.tel')
+  body('borrower.phoneNumber')
     .isString().isLength({ max: 20 }).optional(),
-  body('intermediario.identificacaoIntermed')
+
+  body('intermediary.taxNumber')
     .isString().isLength({ max: 14 }).optional(),
-  body('intermediario.nomeRazaoIntermed')
+  body('intermediary.name')
     .isString().isLength({ max: 150 }).optional(),
-  body('intermediario.cidadeIntermed')
+  body('intermediary.city')
     .isInt().optional(),
-  body('constCivil.codObra')
+  body('construction.workCode')
     .isString().isLength({ max: 30 }).optional(),
-  body('constCivil.art')
+  body('construction.art')
     .isString().isLength({ max: 30 }).optional(),
 ];
 
