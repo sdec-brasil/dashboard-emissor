69fbbe7e861f7290a49f78d0ab35dda3ea52eaf0
David Beyda <dbeyda@globo.com>
2019-09-20 15:30:19 -0300

    [web] Add invoice partial #3
    - Added more fields
    - Automatic fill for calculationBasis field
    - Negative calculationBasis validation not working

--- web/src/components/AddNotaFiscal/index.js
+++ web/src/components/AddNotaFiscal/index.js
@@ -8,8 +8,13 @@ import './style.scss';
 
 
 const AddNotaFiscal = () => {
-  const [data, setData] = useState({ });
-  const [errors, setErrors] = useState({ provision: {} });
+  const [data, setData] = useState({
+    provision: {},
+    tributes: {
+      calculationBasis: '',
+    },
+  });
+  const [errors, setErrors] = useState({ provision: {}, tributes: {} });
   const [emitters, setEmitters] = useState([]);
   const submit = (e) => {
     e.preventDefault();
@@ -50,11 +55,54 @@ const AddNotaFiscal = () => {
     async function getEmitterOptions() {
       const response = await api.get('/v1/user/registered-addresses');
       const addr = response.data;
-      setEmitters(addr.map(a => ({ value: a.address, label: `${a.company.name} - ${a.address}` })))  
-      }
+      setEmitters(addr.map(a => ({ value: a.address, label: `${a.company.name} - ${a.address}` })));
+    }
     getEmitterOptions();
   }, []);
 
+  useEffect(() => {
+    const handleCalculationBasis = (provision = {}, tributes = {}) => {
+      let services = 0;
+      let uncond = 0;
+      let cond = 0;
+      if (provision) {
+        services = parseInt(provision.servicesAmount, 10) || 0;
+      }
+      if (tributes) {
+        uncond = parseInt(tributes.unconditionedDiscountAmount, 10) || 0;
+        cond = parseInt(tributes.conditionedDiscountAmount, 10) || 0;
+      }
+      return (services - uncond - cond);
+    };
+    const value = handleCalculationBasis({ ...data.provision }, { ...data.tributes });
+    setData({
+      ...data,
+      tributes: { ...data.tributes, calculationBasis: value.toString() },
+    });
+  }, [
+    data.provision.servicesAmount,
+    data.tributes.unconditionedDiscountAmount,
+    data.tributes.conditionedDiscountAmount,
+  ]);
+
+  useEffect(() => {
+    if (data.provision.calculationBasis < 0) {
+      console.log('value < 0', data.provision.calculationBasis);
+      const errorMsg = 'A Base de Cálculo está negativa.';
+      writeError(errorMsg, 'servicesAmount', 'provision');
+      writeError(errorMsg, 'unconditionedDiscount', 'tributes');
+      writeError(errorMsg, 'conditionedDiscount', 'tributes');
+    }
+  }, [
+    errors.provision.servicesAmount,
+    errors.tributes.unconditionedDiscountAmount,
+    errors.tributes.conditionedDiscountAmount,
+    data,
+    data.provision,
+    writeError,
+    errors,
+    setErrors,
+  ]);
 
   return (
     <form onSubmit={e => submit(e)} onChange={(e) => { console.log(data); }}>
@@ -137,47 +185,293 @@ const AddNotaFiscal = () => {
             cleanError('servicesAmount', 'provision');
             if (e.target.value < 0) {
               writeError('Números positivos somente.', 'servicesAmount', 'provision');
-              console.log('errors', errors);
             }
-            console.log(this);
             setData({
               ...data,
               provision: { ...data.provision, servicesAmount: e.target.value },
             });
           }}
           />
-        <UikCheckbox label="Incentivo Fiscal"
-          name='tributes.incentivoFiscal'
+        <UikSelect label='Incentivo Fiscal'
+          placeholder="Incentivo Fiscal"
           onChange={e => setData({
             ...data,
-            tributes: { ...data.tributes, incentivoFiscal: e.target.checked },
+            tributes: { ...data.tributes, taxBenefit: e.value },
           })}
-           />
+          options={
+            [
+              { value: true, label: 'Sim' },
+              { value: false, label: 'Não' },
+            ]}
+          />
+        <UikSelect label='Exigibilidade de ISS'
+          placeholder="Exibigilidade"
+          onChange={e => setData({
+            ...data,
+            tributes: { ...data.tributes, issExigibility: e.value },
+          })}
+          options={
+            [
+              { value: 1, label: 'Exigível' },
+              { value: 2, label: 'Não Incidência' },
+              { value: 3, label: 'Isenção' },
+              { value: 4, label: 'Exportação' },
+              { value: 5, label: 'Imunidade' },
+              { value: 6, label: 'Exigiblidade Suspensa por Decisão Judicial' },
+              { value: 7, label: 'Exigibilidade Suspensa por Processo Adminsitrativo' },
+            ]}
+          />
+        <UikInput
+          label="Número do Processo"
+          name='tributes.processNumber'
+          errorMessage={errors.tributes.processNumber}
+          onChange={(e) => {
+            cleanError('processNumber', 'tributes');
+            if (e.target.value.length > 30) {
+              writeError('O número do processo é longo demais (max 30)', 'processNumber', 'tributes');
+            }
+            setData({
+              ...data,
+              provision: { ...data.tributes, processNumber: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Taxa de ISS"
+          name='tributes.issRate'
+          errorMessage={errors.tributes.issRate}
+          onChange={(e) => {
+            cleanError('issRate', 'tributes');
+            if (e.target.value < 0) {
+              writeError('A taxa deve ser um número positivo.', 'issRate', 'tributes');
+            }
+            setData({
+              ...data,
+              provision: { ...data.tributes, issRate: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor do ISS"
+          name='tributes.issAmount'
+          errorMessage={errors.tributes.issAmount}
+          onChange={(e) => {
+            cleanError('issAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'issAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              provision: { ...data.tributes, issAmount: e.target.value },
+            });
+          }}
+          />
+        <UikSelect label='ISS Retido'
+          placeholder="ISS Retido"
+          onChange={e => setData({
+            ...data,
+            tributes: { ...data.tributes, issWithheld: e.value },
+          })}
+          options={
+            [
+              { value: true, label: 'Sim' },
+              { value: false, label: 'Não' },
+            ]}
+          />
+        <UikSelect label='Responsável pela Retenção'
+          placeholder="Responsável"
+          onChange={e => setData({
+            ...data,
+            tributes: { ...data.tributes, retentionResponsible: e.value },
+          })}
+          options={
+            [
+              { value: 1, label: 'Tomador' },
+              { value: 2, label: 'Intermediário' },
+            ]}
+          />
+        <UikSelect label='Regime Especial de Tributação'
+          placeholder="Regime Especial"
+          onChange={e => setData({
+            ...data,
+            tributes: { ...data.tributes, specialTaxRegime: e.value },
+          })}
+          options={
+            [
+              { value: 0, label: 'Tributação Normal' },
+              { value: 1, label: 'Microempresa Municipal' },
+              { value: 2, label: 'Estimativa' },
+              { value: 3, label: 'Sociedade de Profissionais' },
+              { value: 4, label: 'Cooperativa' },
+              { value: 5, label: 'Microempresário Individual (MEI)' },
+              { value: 6, label: 'Microempresário e Empresa de Pequeno Porte (MP EPP)' },
+              { value: 7, label: 'Movimento Mensal/ISS/Fixo Autônomo' },
+              { value: 8, label: 'Sociedade Limitada/Média Empresa' },
+              { value: 9, label: 'Sociedade Anônima/Grande Empresa' },
+              { value: 10, label: 'Empresa Individual de Responsabilidade Limita (EIRELI)' },
+              { value: 11, label: 'Empresa Individual' },
+              { value: 12, label: 'Empresa de Pequeno Porte (EPP)' },
+              { value: 13, label: 'Microempresário' },
+              { value: 14, label: 'Outros/Sem Vínculos' },
+              { value: 15, label: 'Nenhum' },
+              { value: 16, label: 'Nota Avulsa' },
+            ]}
+          />
+        <UikInput
+          type='number'
+          label="Valor do PIS"
+          name='tributes.pisAmount'
+          errorMessage={errors.tributes.pisAmount}
+          onChange={(e) => {
+            cleanError('pisAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'pisAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, pisAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor do COFINS"
+          name='tributes.cofinsAmount'
+          errorMessage={errors.tributes.cofinsAmount}
+          onChange={(e) => {
+            cleanError('cofinsAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'cofinsAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, cofinsAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor do INSS"
+          name='tributes.inssAmount'
+          errorMessage={errors.tributes.inssAmount}
+          onChange={(e) => {
+            cleanError('inssAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'inssAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, inssAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor do IR"
+          name='tributes.irAmount'
+          errorMessage={errors.tributes.irAmount}
+          onChange={(e) => {
+            cleanError('irAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'irAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, irAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor do CSLL"
+          name='tributes.csllAmount'
+          errorMessage={errors.tributes.csllAmount}
+          onChange={(e) => {
+            cleanError('csllAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'csllAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, csllAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor de Desconto Incondicionado"
+          name='tributes.unconditionedDiscountAmount'
+          errorMessage={errors.tributes.unconditionedDiscountAmount}
+          onChange={(e) => {
+            cleanError('unconditionedDiscountAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'unconditionedDiscountAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, unconditionedDiscountAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor de Desconto Condicionado"
+          name='tributes.conditionedDiscountAmount'
+          errorMessage={errors.tributes.conditionedDiscountAmount}
+          onChange={(e) => {
+            cleanError('conditionedDiscountAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'conditionedDiscountAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, conditionedDiscountAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor de outras Retenções"
+          name='tributes.othersAmountsWithheld'
+          errorMessage={errors.tributes.othersAmountsWithheld}
+          onChange={(e) => {
+            cleanError('othersAmountsWithheld', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'othersAmountsWithheld', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, othersAmountsWithheld: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          type='number'
+          label="Valor das Deduções"
+          name='tributes.deductionsAmount'
+          errorMessage={errors.tributes.deductionsAmount}
+          onChange={(e) => {
+            cleanError('deductionsAmount', 'tributes');
+            if (e.target.value < 0) {
+              writeError('O valor deve ser um número positivo.', 'deductionsAmount', 'tributes');
+            }
+            setData({
+              ...data,
+              tributes: { ...data.tributes, deductionsAmount: e.target.value },
+            });
+          }}
+          />
+        <UikInput
+          label="Base de Cálculo"
+          name='tributes.calculationBasis'
+          value={data.tributes.calculationBasis}
+          errorMessage={errors.tributes.calculationBasis}
+          disabled
+          />
 
-
-        {/* body('tributes.incentivoFiscal')
-    .isBoolean(),
-  body('tributes.issExigibility', 'Deve ser um inteiro entre 1 e 7')
-    .isInt({ min: 1, max: 7 }),
-  body('tributes.processNumber', 'Deve ser uma string de até 30 caracteres')
-    .isString().isLength({ max: 30 }).optional(),
-  body('tributes.issRate').isNumeric(),
-  body('tributes.issAmount').isInt(),
-  body('tributes.issWithheld').isBoolean(),
-  body('tributes.retentionResponsible')
-    .isInt({ min: 1, max: 2 })
-    .optional(),
-  body('tributes.specialTaxRegime')
-    .isInt({ min: 0, max: 16 }).optional(),
-  body('tributes.pisAmount').isInt().optional(),
-  body('tributes.cofinsAmount').isInt().optional(),
-  body('tributes.inssAmount').isInt().optional(),
-  body('tributes.irAmount').isInt().optional(),
-  body('tributes.csllAmount').isInt().optional(),
-  body('tributes.unconditionedDiscountAmount').isInt().optional(),
-  body('tributes.conditionedDiscountAmount').isInt().optional(),
-  body('tributes.othersAmountsWithheld').isInt().optional(),
-  body('tributes.deductionsAmount').isInt().optional(),
+        {/*
   body('tributes.calculationBasis', 'O valor esta incorreto').isInt()
     .custom(async (value, { req }) => {
       const data = req.body;
